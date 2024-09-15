b7091360c09c2eb96d1af3872d2363170bfefba4
Saideepak9676 <gsaideepak.sd@gmail.com>
2024-09-15 16:41:32 -0400

    initial commit

---
+++ calculator/__init__.py
@@ -0,0 +1,5 @@
+"""
+Tests for the calculator module.
+"""
+def add(a,b):
+    return a + b
---
+++ tests/test_calculator.py
@@ -0,0 +1,7 @@
+'''Tests for the calculator module'''
+
+from calculator import add
+
+def test_addition():
+    '''Test the addition functionality'''
+    assert add(2,2) == 4
